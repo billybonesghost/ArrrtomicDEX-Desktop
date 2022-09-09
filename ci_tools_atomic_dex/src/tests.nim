9ca4999a5ddbb7094f2c591025c66cbfa5e1c07b
billybonesghost <billybonesghost@proton.me>
2022-09-09 17:47:49 +0800

    I am the captain now

--- ci_tools_atomic_dex/src/tests.nim
+++ ci_tools_atomic_dex/src/tests.nim
@@ -20,7 +20,7 @@ proc run_tests*(build_type: string, osx_sdk_path: string, compiler_path: string)
    
      when defined(linux):
         echo os.getCurrentDir()
-        os.setCurrentDir(os.getCurrentDir().joinPath("bin").joinPath("AntaraAtomicDexTestsAppDir").joinPath("usr").joinPath("bin"))
+        os.setCurrentDir(os.getCurrentDir().joinPath("bin").joinPath("ArrrtomicDEXTestsAppDir").joinPath("usr").joinPath("bin"))
         echo "Running AtomicDex Pro Unit tests"
         discard osproc.execCmd("./" & os.getEnv("DEX_PROJECT_NAME") & "_tests --reporters=xml --out=" & os.getEnv("DEX_PROJECT_NAME") & "-tests-result.xml -s")
         echo "Successfully Generated", os.getEnv("DEX_PROJECT_NAME"), "-tests-result.xml"
